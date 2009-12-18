8d641eab9fa84510ef5104b49cdae43e9c8ea9e1
Gerard Michels <gerard.michels@ou.nl>
2009-12-18 13:50:00 +0000

    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@499 80d59baa-9aef-c140-ba76-e18f3b6eea4d

---
+++ Meterkast/inc/Meterkast.php
@@ -0,0 +1,14 @@
+<?php
+  // Meterkast.php
+  // Generated with ADL vs. 0.8.10-495
+  // Prototype interface design by Sebastiaan JC Joosten (c) Aug 2009
+  
+  
+  error_reporting(E_ALL); 
+  ini_set("display_errors", 1);
+  
+  $content = $_REQUEST['content'];
+  $ctxenv = array();
+  include "$content.php"; 
+  
+  ?>
