3e98d4168d13d3516950a3b5f98e8527221b2bc2
Gerard Michels <gerard.michels@ou.nl>
2012-03-02 16:01:47 +0000

    new atlas w.i.p.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@1771 80d59baa-9aef-c140-ba76-e18f3b6eea4d

---
+++ Atlas/operations.php
@@ -0,0 +1,22 @@
+<?php
+
+$commandstr = array(
+     1 => 'prototype --verbose -p'.COMPILATIONS_PATH.' rap.adl',
+     2 => 'prototype --verbose -o'.FILEPATH.' --export='.$file.' --namespace='.USER.' atlas',
+     3 => 'prototype --verbose -o'.FILEPATH.' --export='.$file.' --namespace='.USER.' atlas',
+     4 => 'ampersand --verbose -o'.COMPILATIONS_PATH.' -f Latex "'.$fullfile.'"',
+     5 => 'prototype --verbose -p'.COMPILATIONS_PATH.'proto/ --theme=student "'.$fullfile.'"',
+     6 => 'ampersand --verbose -o'.COMPILATIONS_PATH.' -f Latex --diagnosis "'.$fullfile.'"',
+     99 => 'prototype --verbose "'.$fullfile.'"'
+     );
+$compileurl = array(
+     1 => COMPILATIONS_PATH.'index.php?interface=Overzicht&atom=1&role=0',
+     2 => 'index.php?file='.$fullfile,
+     3 => 'index.php?file='.$fullfile.'&operation=1',
+     4 => COMPILATIONS_PATH.REQ_OUTPUT,
+     5 => COMPILATIONS_PATH.'proto/index.htm',
+     6 => COMPILATIONS_PATH.REQ_OUTPUT,
+     99 => ''
+     );
+
+?>
