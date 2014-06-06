9e30eb96b7601046e6a382422fb0172f72ecffac
Rieks Joosten <Rieks.Joosten@tno.nl>
2014-06-06 08:16:38 +0000

    Sources zitten nu goed in elkaar. Om het goed te laten werken (met prototype v3.0.1.2964, want de huidige versie is TE onstabiel), is nog een tweak van/met Michiel nodig. Documentatie zou mogelijk ook nog beter kunnen. Feedback is welkom.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@3125 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- EURent/appconfigs/php/plugins/plugin_EURent.php
+++ EURent/appconfigs/php/plugins/plugin_EURent.php
@@ -1,8 +1,5 @@
 <?php 
-/* This file defines a limited number of functions that are being used in the 
-   EURent application.
-*/
-
+// This file defines a limited number of functions that are being used in the EURent application. //
 /*
 VIOLATION (TXT "{EX} InsPair;dateIntervalCompTrigger;Date;", SRC I, TXT ";Date;", TGT I
           ,TXT "{EX} MaxDurationTest;dateIntervalIsWithinMaxRentalDuration"
@@ -35,21 +32,6 @@ function CompRentalCharge($relation,$srcConcept,$srcAtom,$tgtConcept,$arg1,$arg2
    return;
 }
 /*
-VIOLATION (TXT "{EX} CompNrDays" -- Result = 1 + MAX(0, (Actual end date - Actual start date))
-               , TXT ";computedRentalPeriod;CompNrDays;", SRC I, TXT ";Integer"
-               , TXT ";", SRC latestDate -- = Actual end date
-               , TXT ";", SRC earliestDate -- = Actual start date
-          )
-implemented function is ok.
-*/
-function CompNrDays($relation,$srcConcept,$srcAtom,$tgtConcept,$earliestDate,$latestDate)
-{  emitLog("CompNrDays($relation,$srcConcept,$srcAtom,$tgtConcept,$earliestDate,$latestDate)");
-   $datediff = strtotime($latestDate) - strtotime($earliestDate);
-   $result = 1 + max(0, floor($datediff/(60*60*24)));
-   InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$result);
-   return;
-}
-/*
 VIOLATION (TXT "{EX} CompTariffedCharge" -- result  := integer * amount 
                , TXT ";computedTariffedCharge;CompTariffedCharge;", SRC I, TXT ";Amount"
                , TXT ";", SRC ctcNrOfDays
@@ -59,20 +41,6 @@ VIOLATION (TXT "{EX} CompTariffedCharge" -- result  := integer * amount
 function CompTariffedCharge($relation,$srcConcept,$srcAtom,$tgtConcept,$ctcNrOfDays,$ctcDailyAmount)
 {  emitLog("CompTariffedCharge($relation,$srcConcept,$srcAtom,$tgtConcept,$ctcNrOfDays,$ctcDailyAmount)");
    $result = $ctcNrOfDays * $ctcDailyAmount;
-   InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$result);
-   return;
-}
-/*
-VIOLATION (TXT "{EX} CompNrExcessDays"  -- Result = MAX(0, (Actual end date - Contracted end date))
-               , TXT ";computedNrOfExcessDays;CompNrExcessDays;", SRC I, TXT ";Integer"
-               , TXT ";", SRC lastDate
-               , TXT ";", SRC firstDate
-          )
-*/
-function CompNrExcessDays($relation,$srcConcept,$srcAtom,$tgtConcept,$firstDate,$lastDate)
-{  emitLog("CompNrExcessDays($relation,$srcConcept,$srcAtom,$tgtConcept,$firstDate,$lastDate)");
-   $datediff = strtotime($lastDate) - strtotime($firstDate);
-   $result = max(0, floor($datediff/(60*60*24)));
    InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$result);
    return;
 }
