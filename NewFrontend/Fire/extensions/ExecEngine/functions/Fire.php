bffd725f7925ad786b772403b3203c3883bd39e0
Michiel-s <michielstornebrink@gmail.com>
2015-02-10 14:42:17 +0100

    Fixed syntax errors in ExecEngine functions for Fire app

--- NewFrontend/Fire/extensions/ExecEngine/functions/Fire.php
+++ NewFrontend/Fire/extensions/ExecEngine/functions/Fire.php
@@ -1,11 +1,11 @@
 <?php 
 
-logEvent($logtext) 
+function logEvent($logtext) 
 { $date = date('Y-m-d H:i:s');
   Notifications::addLog("$date - $logtext");
 }
 
-CloseFireDoor($FireDoorConcept;$FireDoorAtom)
+function CloseFireDoor($FireDoorConcept,$FireDoorAtom)
 {  // code (to simulate) closing a firedoor, given its ID
    logEvent("Firedoor $FireDoorAtom has been instructed to close.");
 }
