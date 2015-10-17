47d32ee376c83a505c4101b8085b8f16ed99257b
Rieks <rieks.joosten@tno.nl>
2015-10-17 04:54:29 +0200

    Asjeblieft

---
+++ MDT/include/extensions/ExecEngine/functions/plugin_Counters.php
@@ -0,0 +1,29 @@
+<?php 
+// Use:  VIOLATION (TXT "{EX} Increment;<relation>;<srcConcept>;<srcAtom>;<tgtConcept>;<tgtAtom>")
+// This function increments tgtAtom, setting it to '1' if it does not exist.
+function Increment($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom)
+{  if ($tgtAtom === "") $tgtAtom=0;
+   $tgtAtom = $tgtAtom + 1;
+   InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom);
+   return;
+}
+
+// Use:  VIOLATION (TXT "{EX} Decrement;<relation>;<srcConcept>;<srcAtom>;<tgtConcept>;<tgtAtom>")
+// This function decrements tgtAtom, logging an error if it does not exist or drops below zero.
+function Decrement($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom)
+{  if ($tgtAtom === "") $tgtAtom=0;
+   if ($tgtAtom < 1) throw new Exception ("Decrement below zero: $relation, $srcConcept, $srcAtom, $tgtConcept, $tgtAtom");
+   $tgtAtom = $tgtAtom - 1;
+   InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom);
+   return;
+}
+
+// Use:  VIOLATION (TXT "{EX} Percentage;<relation>;<srcConcept>;<srcAtom>;<tgtConcept>;<teller>;<noemer>")
+// This function computers 100*<teller>/<noemer>
+function Percentage($relation,$srcConcept,$srcAtom,$tgtConcept,$teller,$noemer)
+{  if ($noemer == 0) throw new Exception ("Cannot divide by zero: $teller, $noemer");
+   $tgtAtom = number_format((100*$teller) / $noemer, 0);
+   InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom);
+   return;
+}
+?>
