a02f4e6d905aa6e9e8ca6ce8179a4cc4abf261bb
Stef Joosten <stef.joosten@ou.nl>
2016-07-15 14:55:09 +0200

    display compiler results as .pdf file in RAP3

--- RAP3/include/extensions/ExecEngine/functions/cli.php
+++ RAP3/include/extensions/ExecEngine/functions/cli.php
@@ -4,20 +4,77 @@ use Ampersand\Log\Logger;
 use Ampersand\Config;
 use Ampersand\Core\Relation;
 use Ampersand\Core\Atom;
+use Ampersand\Core\Concept;
 
-function CompileWithAmpersand($file, $scriptAtomId){
-	Logger::getLogger('EXECENGINE')->info("CompileWithAmpersand({$file})");
-	$relCompileResponse = Relation::getRelation('compileresponse','Script','CompileResponse');
-	$scriptAtom = new Atom($scriptAtomId,'Script');
+function CompileWithAmpersand($action, $file, $scriptAtomId){
+	Logger::getLogger('EXECENGINE')->info("CompileWithAmpersand({$action}, {$file}, {$scriptAtomId})");
+	
 	$path = realpath(Config::get('absolutePath') . $file);
+	$scriptAtom = new Atom($scriptAtomId,'Script');
+	
+	switch($action){
+		case 'compilecheck' : 
+			CompileCheck($path, $scriptAtom);
+			break;
+		case 'fspec' : 
+			FuncSpec($path, $scriptAtom);
+			break;
+		case 'diagnose':
+			break;
+		case 'proto' : 
+			break;
+		default :
+			break;
+	}
+}
+
+function CompileCheck($path, $scriptAtom){
 	$cmd = "Ampersand {$path}";
 	Logger::getLogger('COMPILEENGINE')->debug("cmd:'{$cmd}'");
-	$output = array();
-	$response = exec($cmd, $output);
-	Logger::getLogger('COMPILEENGINE')->debug("response:'{$response}'");
-	$responseAtom = new Atom(implode('\n',$output),'CompileResponse');
+	
+	// Execute cmd
+	Execute($cmd, $response, $exitcode);
+	
+	// Add response to database
+	$relCompileResponse = Relation::getRelation('compileresponse','Script','CompileResponse');
+	$responseAtom = new Atom($response,'CompileResponse');
 	$relCompileResponse->addLink($scriptAtom,$responseAtom,false,'COMPILEENGINE');
-	Logger::getLogger('COMPILEENGINE')->debug($output);
+	
+	// If exitcode = 0, set property 'scriptOk[Script*Script] [PROP]'
+	if($exitcode == 0){
+		//$relScriptOk = Relation::getRelation('scriptOk','Script','Script');
+		//$relScriptOk->addLink($scriptAtom,$scriptAtom,false,'COMPILEENGINE');
+	}
+	
+}
+
+function FuncSpec($path, $scriptAtom){
+	$dir = dirname($path);
+	$extension = pathinfo($path, PATHINFO_EXTENSION);
+	$filename = pathinfo($path, PATHINFO_FILENAME);
+	
+	$cmd = "Ampersand {$path} -fl --language=NL --outputDir=\"{$dir}/fspec\" --verbose";
+	Logger::getLogger('COMPILEENGINE')->debug("cmd:'{$cmd}'");
+
+	// Execute cmd
+	Execute($cmd, $response, $exitcode);
+	
+	// Create FileObject in database
+	$concept = Concept::getConcept('FileObject');
+	$fspecAtom = $concept->createNewAtom();
+	Relation::getRelation('filePath','FileObject','FilePath')->addLink($fspecAtom, new Atom("uploads/fspec/{$filename}.pdf", 'FilePath'));
+	Relation::getRelation('originalFileName','FileObject','FileName')->addLink($fspecAtom, new Atom('LogicalDataModel.pdf', 'FileName'));
+
+	// Link fSpecAtom to scriptAtom
+	Relation::getRelation('funcSpec','Script','FileObject')->addLink($scriptAtom,$fspecAtom,false,'COMPILEENGINE');
+	
+}
+
+function Execute($cmd, &$response, &$exitcode){
+	$output = array();
+	exec($cmd, $output, $exitcode);
+	$response = implode('\n',$output);
+	Logger::getLogger('COMPILEENGINE')->debug("response:'{$response}'");
 }
 
 ?>
