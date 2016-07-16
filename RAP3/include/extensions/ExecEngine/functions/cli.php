11e00d8ddfd98b042a3b5584b120d12cda45b8ee
Rieks <rieks.joosten@tno.nl>
2016-07-16 07:49:04 +0200

    RAP3 - restructuring of stuff, added preliminary code for diagnosis and prototypes

--- RAP3/include/extensions/ExecEngine/functions/cli.php
+++ RAP3/include/extensions/ExecEngine/functions/cli.php
@@ -19,11 +19,14 @@ function CompileWithAmpersand($action, $file, $scriptAtomId){
 		case 'fspec' : 
 			FuncSpec($path, $scriptAtom);
 			break;
-		case 'diagnose':
+		case 'diagnosis':
+			Diagnosis($path, $scriptAtom);
 			break;
-		case 'proto' : 
+		case 'prototype' : 
+			Prototype($path, $scriptAtom);
 			break;
 		default :
+			Logger::getLogger('EXECENGINE')->error("Unknown action ({$action}) specified");
 			break;
 	}
 }
@@ -32,19 +35,13 @@ function CompileCheck($path, $scriptAtom){
 	$cmd = "Ampersand {$path}";
 	Logger::getLogger('COMPILEENGINE')->debug("cmd:'{$cmd}'");
 	
-	// Execute cmd
-	Execute($cmd, $response, $exitcode);
+	// Execute cmd, and populate 'scriptOk' upon success
+	Execute($cmd, $response, $exitcode, 'scriptOk', $scriptAtom);
 	
 	// Add response to database
 	$relCompileResponse = Relation::getRelation('compileresponse','Script','CompileResponse');
 	$responseAtom = new Atom($response,'CompileResponse');
 	$relCompileResponse->addLink($scriptAtom,$responseAtom,false,'COMPILEENGINE');
-	
-	// If exitcode = 0, set property 'scriptOk[Script*Script] [PROP]'
-	if($exitcode == 0){
-		//$relScriptOk = Relation::getRelation('scriptOk','Script','Script');
-		//$relScriptOk->addLink($scriptAtom,$scriptAtom,false,'COMPILEENGINE');
-	}
 	
 }
 
@@ -56,23 +53,64 @@ function FuncSpec($path, $scriptAtom){
 	$cmd = "Ampersand {$path} -fl --language=NL --outputDir=\"{$dir}/fspec\" --verbose";
 	Logger::getLogger('COMPILEENGINE')->debug("cmd:'{$cmd}'");
 
-	// Execute cmd
-	Execute($cmd, $response, $exitcode);
-	
+	// Execute cmd, and populate 'funcSpecOk' upon success
+	Execute($cmd, $response, $exitcode, 'funcSpecOk', $scriptAtom);
+
 	// Create FileObject in database
 	$concept = Concept::getConcept('FileObject');
-	$fspecAtom = $concept->createNewAtom();
-	Relation::getRelation('filePath','FileObject','FilePath')->addLink($fspecAtom, new Atom("uploads/fspec/{$filename}.pdf", 'FilePath'));
-	Relation::getRelation('originalFileName','FileObject','FileName')->addLink($fspecAtom, new Atom('LogicalDataModel.pdf', 'FileName'));
+	$fileObjectAtom = $concept->createNewAtom();
+	Relation::getRelation('filePath','FileObject','FilePath')->addLink($fileObjectAtom, new Atom("uploads/fspec/{$filename}.pdf", 'FilePath'));
+	Relation::getRelation('originalFileName','FileObject','FileName')->addLink($fileObjectAtom, new Atom('LogicalDataModel.pdf', 'FileName'));
 
 	// Link fSpecAtom to scriptAtom
-	Relation::getRelation('funcSpec','Script','FileObject')->addLink($scriptAtom,$fspecAtom,false,'COMPILEENGINE');
+	Relation::getRelation('funcSpec','Script','FileObject')->addLink($scriptAtom,$fileObjectAtom,false,'COMPILEENGINE');
 	
 }
 
-function Execute($cmd, &$response, &$exitcode){
+function Diagnosis($path, $scriptAtom){
+	$dir = dirname($path);
+	$extension = pathinfo($path, PATHINFO_EXTENSION);
+	$filename = pathinfo($path, PATHINFO_FILENAME);
+	
+	$cmd = "Ampersand {$path} --diagnosis --language=NL --outputDir=\"{$dir}/diag\" --verbose";
+	Logger::getLogger('COMPILEENGINE')->debug("cmd:'{$cmd}'");
+
+	// Execute cmd, and populate 'diagOk' upon success
+	Execute($cmd, $response, $exitcode, 'diagOk', $scriptAtom);
+	
+	// Create FileObject in database
+	$concept = Concept::getConcept('FileObject');
+	$fileObjectAtom = $concept->createNewAtom();
+	Relation::getRelation('filePath','FileObject','FilePath')->addLink($fileObjectAtom, new Atom("uploads/diag/{$filename}.pdf", 'FilePath'));
+	Relation::getRelation('originalFileName','FileObject','FileName')->addLink($fileObjectAtom, new Atom('Diagnosis.pdf', 'FileName'));
+
+	// Link diagAtom to scriptAtom
+	Relation::getRelation('diag','Script','FileObject')->addLink($scriptAtom,$fileObjectAtom,false,'COMPILEENGINE');
+	
+}
+
+function Prototype($path, $scriptAtom){
+	$dir = dirname($path);
+	$extension = pathinfo($path, PATHINFO_EXTENSION);
+	$filename = pathinfo($path, PATHINFO_FILENAME);
+	
+	$cmd = "Ampersand {$path} --proto=\"{$dir}/prototype\" --language=NL --verbose";
+	Logger::getLogger('COMPILEENGINE')->debug("cmd:'{$cmd}'");
+
+	// Execute cmd, and populate 'protoOk' upon success
+	Execute($cmd, $response, $exitcode, 'protoOk', $scriptAtom);
+	
+}
+
+function Execute($cmd, &$response, &$exitcode, $proprelname, $scriptAtom){
 	$output = array();
 	exec($cmd, $output, $exitcode);
+
+	// Set property '$proprelname[Script*Script] [PROP]' depending on the exit code
+	$proprel = Relation::getRelation($proprelname,'Script','Script');
+	$proprel->addLink($scriptAtom,$scriptAtom,($exitcode == 0),'COMPILEENGINE');
+
+	// format execution output
 	$response = implode('\n',$output);
 	Logger::getLogger('COMPILEENGINE')->debug("response:'{$response}'");
 }
