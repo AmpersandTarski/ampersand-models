7192bb6dc37f3079b3eb17ec10b5dc19f3ebef2e
Rieks <rieks.joosten@tno.nl>
2017-01-05 12:12:06 +0100

    TText - added interfacing for (manual!) computations

--- TText/include/extensions/ExecEngine/functions/TTextFunctions.php
+++ TText/include/extensions/ExecEngine/functions/TTextFunctions.php
@@ -1,28 +1,27 @@
 <?php
 use Ampersand\Log\Logger;
 
-// Following function parses $parsetext, splitting it into text-sections and TText names.
-// {EX} ParseTemplatePhrase;ttPlaceholders;ttIsUsedBy;", SRC I, TXT ";ttName;", TGT I
-function ParseTemplatePhrase($rel        // the relation name that will contain the placeholder names
-                            ,$SrcConcept // the SRC concept of said relation
-                            ,$CritAtom   // the criterion-atom of which the text is to be parsed
-                            ,$TgtConcept // the TGT concept of said relation
-                            ,$parsetext  // the criterion-text to be parsed
+// Following function parses $parsetext, splitting it into text-sections and placeholders (TText names).
+// {EX} ParseTemplatePhrase;ttPlaceholders;TText;", SRC I, TXT ";TTName;", SRC ttTemplate
+function ParseTemplatePhrase($rel        // the relation name that contains the placeholder names
+                            ,$SrcConcept // the SRC concept of $rel (i.e. TText)
+                            ,$SrcAtom    // the (TText) Atom whose template text is to be parsed
+                            ,$TgtConcept // the TGT concept of $rel (i.e. TTName)
+                            ,$parsetext  // the template text that needs to be parsed
                             )
-{	Logger::getLogger('EXECENGINE')->debug("-- ParseTemplatePhrase($rel,$SrcConcept,$CritAtom,$TgtConcept,$parsetext)");
-	$itemizedText = $parsetext;
-    while (strlen($parsetext))
-	{ 	if ($parsetext[0] == '[') // Customize the Item as either a TText or a text
-		{	// handle TText-names
-			if (strpos($parsetext, ']') === false) break; // TText names must be properly terminated with a ']' character
+{	Logger::getLogger('EXECENGINE')->debug("-- ParseTemplatePhrase($rel,$SrcConcept,$SrcAtom,$TgtConcept,$parsetext)");
+    while (strlen($parsetext)) // Parce the remainder of the template text until it is empty.
+	{ 	if ($parsetext[0] == '[') // Found a placeholder (i.e. the TTName of some TText)
+		{	// handle placeholder
+			if (strpos($parsetext, ']') === false) break; // Placeholders are terminated with a ']' character
 			$chars = substr($parsetext, 1, strpos($parsetext, ']')-1); // the name of the TText is within '[' and ']'.
 			$parsetext = substr($parsetext, strpos($parsetext, ']')+1); // set the remainder of the text to be parsed
  //			Logger::getLogger('EXECENGINE')->debug("ParseTemplatePhrase - create VAR item $chars");
-			InsPair($rel,$SrcConcept,$CritAtom,$TgtConcept,$chars);
+			InsPair($rel,$SrcConcept,$SrcAtom,$TgtConcept,$chars);
 		} else
-		{	// handle phrase-texts
-			$charspos = strpos($parsetext,'[');
-			if ($charspos) // get the text up to the end or the next TText
+		{	// handle non-placeholder texts
+			$charspos = strpos($parsetext,'['); // see if there is a placeholder in the (remaining) text.
+			if ($charspos) // get the text up to the end or the next placeholder
 			{	$chars = substr($parsetext, 0, $charspos);
 			}else
 			{	$chars = substr($parsetext, 0);
