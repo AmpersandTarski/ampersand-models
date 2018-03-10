41e5d27b913a250aabd905936ba9494ccc6786c7
Rieks <rieks.joosten@tno.nl>
2018-03-10 14:45:55 +0100

    TText - changes/cleanup for refactorization - Users will need to revise their TText_Module.adl (see: TText_ModuleExample.adl)

--- TText/customizations/localSettings.php
+++ TText/customizations/localSettings.php
@@ -2,35 +2,32 @@
 
 use Ampersand\Log\Logger;
 use Ampersand\Log\NotificationHandler;
-use Ampersand\Config;
+use Ampersand\Misc\Config;
+use Ampersand\Plugs\MysqlDB\MysqlDB;
+// use Ampersand\Rule\ExecEngine;
 
-define ('LOCALSETTINGS_VERSION', 1.5);
+define ('LOCALSETTINGS_VERSION', 1.6);
 
-date_default_timezone_set('Europe/Amsterdam');
+date_default_timezone_set('Europe/Amsterdam'); // See http://php.net/manual/en/timezones.php for a list of supported timezones
 
 /**************************************************************************************************
  * LOGGING functionality
  *************************************************************************************************/
 error_reporting(E_ALL & ~E_NOTICE);
-ini_set("display_errors", true);
+ini_set("display_errors", false);
 
 /**************************************************************************************************
  * Execution time limit is set to a default of 30 seconds. Use 0 to have no time limit. (not advised)
  *************************************************************************************************/
 set_time_limit (30);
 
-/**************************************************************************************************
- * specifies whether changes in interface are directly communicated (saved) to server (default = true)
- * the user can change this setting from the menu bar 
- *************************************************************************************************/
-Config::set('interfaceAutoSaveChanges', 'transactions', true); 
-
-Config::set('debugMode', 'global', true); // default = false
+Config::set('debugMode', 'global', true); // default mode = false
 
 // Log file handler
-$fileHandler = new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/log/error.log', 0, \Monolog\Logger::WARNING);
+$fileHandler = new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/log/error.log', 0, \Monolog\Logger::DEBUG);
 //$fileHandler->pushProcessor(new \Monolog\Processor\WebProcessor()); // Adds IP adres and url info to log records
-Logger::registerGenericHandler($fileHandler);
+$wrapper = new \Monolog\Handler\FingersCrossedHandler($fileHandler, \Monolog\Logger::ERROR, 0, true, true, \Monolog\Logger::WARNING);
+Logger::registerGenericHandler($wrapper);
 
 if(Config::get('debugMode')){
     $fileHandler = new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/log/debug.log', 0, \Monolog\Logger::DEBUG);
@@ -51,18 +48,23 @@ Logger::registerHandlerForChannel('USERLOG', new NotificationHandler(\Monolog\Lo
 /**************************************************************************************************
  * SERVER settings
  *************************************************************************************************/
-// Config::set('serverURL', 'global', 'http://www.yourdomain.nl'); // defaults to http://localhost/<ampersand context name>
+Config::set('serverURL', 'global', 'http://localhost/MPTrx'); // defaults to http://localhost/<ampersand context name>
 // Config::set('apiPath', 'global', '/api/v1'); // relative path to api
 
 
 /**************************************************************************************************
  * DATABASE settings
  *************************************************************************************************/
-// Config::set('dbHost', 'mysqlDatabase', 'localhost');
-// Config::set('dbUser', 'mysqlDatabase', 'ampersand');
-// Config::set('dbPassword', 'mysqlDatabase', 'ampersand');
-// Config::set('dbName', 'mysqlDatabase', '');
-
+$container['mysql_database'] = function($c) {
+    $dbHost = Config::get('dbHost', 'mysqlDatabase');
+    $dbUser = Config::get('dbUser', 'mysqlDatabase');
+    $dbPass = Config::get('dbPassword', 'mysqlDatabase');
+    $dbName = Config::get('dbName', 'mysqlDatabase');
+    return new MysqlDB($dbHost, $dbUser, $dbPass, $dbName, Logger::getLogger('DATABASE'));
+};
+$container['default_plug'] = function ($c) {
+    return $c['mysql_database'];
+};
 
 /**************************************************************************************************
  * LOGIN FUNCTIONALITY
@@ -74,13 +76,20 @@ Logger::registerHandlerForChannel('USERLOG', new NotificationHandler(\Monolog\Lo
  * Uncomment the config setting below
  *************************************************************************************************/
 Config::set('loginEnabled', 'global', true);
+// Config::set('allowedRolesForImporter', 'global', []); // list of roles that have access to the importer
+
+
+/**************************************************************************************************
+ * EXEC ENGINE 
+ *************************************************************************************************/
+// Config::set('execEngineRoleNames', 'execEngine', ['ExecEngine']);
+// Config::set('autoRerun', 'execEngine', true);
+// Config::set('maxRunCount', 'execEngine', 10);
+chdir(__DIR__);
+foreach(glob('execfunctions/*.php') as $filepath) require_once(__DIR__ . DIRECTORY_SEPARATOR . $filepath);
 
 
 /**************************************************************************************************
  * EXTENSIONS
  *************************************************************************************************/
-require_once(__DIR__ . '/extensions/ExecEngine/ExecEngine.php'); // Enable ExecEngine
-require_once(__DIR__ . '/extensions/ExcelImport/ExcelImport.php'); // Enable ExcelImport
 
-
-?>
